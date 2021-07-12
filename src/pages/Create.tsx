ba000eec7df72d04f9bc362e369466ca76e900cc
Christian Acosta <cta@christiantacosta.com>
2021-07-12 00:21:46 -0400

    Demoed Typography component and installed typescript

---
+++ src/pages/Create.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+export default function Create() {
+  return (
+    <div>
+      <Typography
+      variant="h6"
+      color="textSecondary"
+      component="h2"
+      gutterBottom
+      > 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsam deleniti iure beatae aliquam facilis maxime nemo totam quibusdam assumenda atque quam. Rem fuga optio ducimus provident molestiae numquam iure!
+      </Typography>
+    </div>
+  );
+}
